7af4007474fd0e1fe6e92e5ebecee22d9114bfd6
Tolesa11fikadu <tolee4530@gmail.com>
2023-06-04 14:12:01 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """index"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cis_fun(text):
+    """cis_fun"""
+    s = text
+    s = s.replace("_", " ")
+    return "C {}".format(s)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
